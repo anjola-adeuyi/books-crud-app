63f145c9f7be1009715049581660888b8b4fecf6
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-11-08 23:17:28 +0100

    setup add form

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const Add = () => {
+  const [book, setBook] = React.useState({
+    title: '',
+    desc: '',
+    price: '',
+    cover: '',
+  });
+  
   return (
-    <div>Add</div>
+    <div>
+      <h1>Add Book</h1>
+      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Description" />
+      <input type="number" placeholder="Price" />
+      <input type="text" placeholder="Cover" />
+    </div>
   )
 }
 
