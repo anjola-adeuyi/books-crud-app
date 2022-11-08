df85e424fc179ff6ad0550ed46be4a3f6c4af65a
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-11-08 23:14:03 +0100

    update book schema

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Books = () => {
+  const [books, setBooks] = React.useState([])
+
+  React.useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await axios.get('http://localhost:8800/books')
+        console.log('first response', response);
+        setBooks(response.data)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchBooks()
+  }, []);
+
   return (
-    <div>Books</div>
+    <div>
+      <h1>Jola Books Shop</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book?.id}>
+            {book?.cover && <img src={book?.cover} alt={book?.title} />}
+            <h2>{book?.title}</h2>
+            <p>{book?.desc}</p>
+            <span>{book?.price}</span>
+          </li>
+        ))}
+      </ul>
+      <button>
+        <Link to="/add">Add Book</Link>
+      </button>
+    </div>
   )
 }
 
