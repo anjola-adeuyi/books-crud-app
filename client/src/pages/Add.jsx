4b16c71b9e30c0f57e552447bf9015b68becf936
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-11-08 23:21:21 +0100

    handle input change

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -4,17 +4,24 @@ const Add = () => {
   const [book, setBook] = React.useState({
     title: '',
     desc: '',
-    price: '',
+    price: null,
     cover: '',
   });
-  
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBook({ ...book, [name]: value });
+  };
+
+  console.log('first book', book);
+
   return (
     <div>
       <h1>Add Book</h1>
-      <input type="text" placeholder="Title" />
-      <input type="text" placeholder="Description" />
-      <input type="number" placeholder="Price" />
-      <input type="text" placeholder="Cover" />
+      <input type="text" placeholder="Title" onChange={handleChange} name="title" />
+      <input type="text" placeholder="Description" onChange={handleChange} name="desc" />
+      <input type="number" placeholder="Price" onChange={handleChange} name="price" />
+      <input type="text" placeholder="Cover" onChange={handleChange} name="cover" />
     </div>
   )
 }
