2fce2218777db915897be983bd9638e67e17fd2a
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-11-08 22:29:35 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import mysql from 'mysql';
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'anjolaoluwa',
+  database: 'my-books'
+});
+
+app.use(express.json()); 
+
+app.get('/', (req, res) => {
+  res.send('Hello from crud app!');
+});
+
+app.get('/books', (req, res) => {
+  const sql = 'SELECT * FROM books';
+  connection.query(sql, (err, rows) => {
+    console.log('rows', rows, err);
+    if (err) {
+      res.status(500).send('An error occurred', err);
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.post('/books', (req, res) => {
+  const sql = 'INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)';
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.cover,
+  ]
+  connection.query(sql, [values], (err, result) => {
+    if (err) {
+      res.status(500).send('An error occurred', err);
+    } else {
+      res.send('Book successfully added!');
+      res.json(result);
+    }
+  });
+});
+
+app.listen(8800, () => {
+  console.log('Server is running on port 8800');
+});
