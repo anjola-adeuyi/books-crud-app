0da7deda3dae700a9fc2522e4e486761a9f9e5c1
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-11-19 22:59:56 +0100

    todo: put route

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,10 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'anjolaoluwa',
-  database: 'my-books'
+  database: 'my-books',
 });
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -32,12 +32,7 @@ app.get('/books', (req, res) => {
 
 app.post('/books', (req, res) => {
   const sql = 'INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)';
-  const values = [
-    req.body.title,
-    req.body.desc,
-    req.body.price,
-    req.body.cover,
-  ]
+  const values = [req.body.title, req.body.desc, req.body.price, req.body.cover];
   connection.query(sql, [values], (err, result) => {
     if (err) {
       res.status(500).send('An error occurred', err);
@@ -47,6 +42,8 @@ app.post('/books', (req, res) => {
     }
   });
 });
+
+// TODO: add a PUT route to update a book
 
 app.listen(8800, () => {
   console.log('Server is running on port 8800');
