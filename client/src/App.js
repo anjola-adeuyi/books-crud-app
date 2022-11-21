f52c3a279d8188c9618b5be364d91454c612a03c
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-11-22 00:13:04 +0100

    add styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Add, Books, Update } from './pages';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="/update" element={<Update />} />
+          <Route
+            path="/"
+            element={<Books />}
+          />
+          <Route
+            path="/add"
+            element={<Add />}
+          />
+          <Route
+            path="/update"
+            element={<Update />}
+          />
         </Routes>
       </Router>
     </div>
